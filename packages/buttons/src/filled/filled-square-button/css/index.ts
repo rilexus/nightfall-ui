d6be475368d3167fbe16b3971ba066655e8c9ac3
rilexus <stanip@hotmail.de>
2022-01-24 20:21:23 +0100

    refactor(buttons): flashing color

--- packages/buttons/src/filled/filled-square-button/css/index.ts
+++ packages/buttons/src/filled/filled-square-button/css/index.ts
@@ -2,8 +2,9 @@ import { css } from "styled-components";
 
 const filledButtonClickAnimation = css`
   transition: background-color 200ms ease 0ms;
+
   &:active {
-    background-color: white;
+    background-color: #e1e1e1;
   }
 `;
 
